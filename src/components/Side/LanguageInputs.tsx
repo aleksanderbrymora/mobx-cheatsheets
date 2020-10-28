0b8818f6456687124dc1ef5fca572f2695d0db37
Aleksander Brymora <aleksander.australia@gmail.com>
2020-10-28 23:31:25 +1100

    initial structure of the component

--- src/components/Side/LanguageInputs.tsx
+++ src/components/Side/LanguageInputs.tsx
@@ -1,7 +1,45 @@
+import { Box, Flex, Input } from '@chakra-ui/core';
 import React from 'react';
+import { useMst } from 'src/models/Root';
+import { capitalise } from 'src/utils/capitalise';
 
 const LanguageInputs = () => {
-	return <div>LanguageInputs</div>;
+	const {
+		sheet: { changeFromLanuage, changeToLanuage, fromLang, toLang },
+	} = useMst();
+	return (
+		<Flex my='1rem'>
+			<LanguageInput
+				name='from'
+				onChange={changeFromLanuage}
+				value={fromLang}
+			/>
+			<LanguageInput name='to' onChange={changeToLanuage} value={toLang} />
+		</Flex>
+	);
+};
+
+interface LanguageProps {
+	name: 'from' | 'to';
+	onChange: (to: string) => void;
+	value: string;
+}
+
+const LanguageInput: React.FC<LanguageProps> = ({ name, onChange, value }) => {
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		onChange(e.target.value);
+	};
+
+	return (
+		<Box>
+			<label htmlFor={`language-input-${name}`}>{capitalise(name)}</label>
+			<Input
+				onChange={handleChange}
+				value={value}
+				placeholder={capitalise(name)}
+			/>
+		</Box>
+	);
 };
 
 export default LanguageInputs;
