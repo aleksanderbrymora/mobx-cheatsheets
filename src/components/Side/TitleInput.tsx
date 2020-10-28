46fe1bea084c8ac3fac7abe24e2dbe2f6e89aab9
Aleksander Brymora <aleksander.australia@gmail.com>
2020-10-28 22:06:24 +1100

    Title input finished

--- src/components/Side/TitleInput.tsx
+++ src/components/Side/TitleInput.tsx
@@ -1,7 +1,25 @@
+import { Box, Input } from '@chakra-ui/core';
 import React from 'react';
+import { useMst } from 'src/models/Root';
 
 const TitleInput = () => {
-	return <div>TitleInput</div>;
+	const { sheet } = useMst();
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		sheet.changeTitle(e.target.value);
+	};
+
+	return (
+		<Box mb='1rem'>
+			<label htmlFor='title-input'>Title</label>
+			<Input
+				id='title-input'
+				onChange={handleInputChange}
+				placeholder='Title of the cheat sheet'
+				value={sheet.title}
+			/>
+		</Box>
+	);
 };
 
 export default TitleInput;
