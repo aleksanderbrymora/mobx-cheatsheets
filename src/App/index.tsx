3d265ff06dd2048417344529b91102f94e9f39e1
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-04 22:25:47 +1100

    added homepage

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,11 +2,15 @@ import { observer } from 'mobx-react';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from 'src/pages/create';
+import Home from 'src/pages/home';
 
 const App = observer(() => {
 	return (
 		<Router>
 			<Switch>
+				<Route path='/'>
+					<Home />
+				</Route>
 				<Route path='/create'>
 					<Create />
 				</Route>
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import { Heading } from '@chakra-ui/core';
+import React from 'react';
+
+const Home = () => {
+	return <Heading>Home</Heading>;
+};
+
+export default Home;
