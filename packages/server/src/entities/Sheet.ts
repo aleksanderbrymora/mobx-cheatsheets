8888849c98de2bb949f14575ac01b7db2950b617
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-06 22:49:47 +1100

    updated sheet with typegraphql decorators

--- packages/server/src/entities/Sheet.ts
+++ packages/server/src/entities/Sheet.ts
@@ -1,3 +1,4 @@
+import { Field, Int } from 'type-graphql';
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
 import { Book } from './Book';
 import { MetaWithLanguages } from './shared/MetaWithLanguages';
@@ -6,25 +7,32 @@ import { User } from './User';
 
 @Entity()
 export class Sheet extends MetaWithLanguages {
+	@Field()
 	@Column()
 	title: string;
 
+	@Field()
 	@Column({ default: true })
 	containsProfanity: boolean;
 
+	@Field((_type) => Int)
 	@Column('int')
 	points: number;
 
+	@Field((_type) => User)
 	@ManyToOne(() => User, (user) => user.sheets)
 	createdBy: User;
 
+	@Field((_type) => User)
 	@ManyToOne(() => User, (user) => user.forkedSheets, { nullable: true })
 	forkedFrom: User;
 
+	@Field((_type) => [TranslationGroup])
 	@ManyToMany(() => TranslationGroup)
 	@JoinTable()
 	translationGroups: TranslationGroup[];
 
+	@Field((_type) => [Book])
 	@ManyToMany(() => Book, (book) => book.sheets)
 	books: Book[];
 }
