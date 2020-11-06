8f0293b97f3a40359635ec36e9ff7a15bfb4280b
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-07 00:33:22 +1100

    added query for single sheet

--- packages/server/src/resolvers/SheetResolver.ts
+++ packages/server/src/resolvers/SheetResolver.ts
@@ -1,10 +1,11 @@
 import { Sheet } from '../entities/Sheet';
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Query, Resolver } from 'type-graphql';
 
 @Resolver(Sheet)
 export class SheetResolver {
-	@Query((_returns) => String)
-	sheet() {
-		return 'hey';
+	@Query((_returns) => Sheet, { nullable: true })
+	async sheet(@Arg('id') id: string) {
+		const sheet = await Sheet.findOne(id);
+		return sheet;
 	}
 }
