a4b3516c135de345cfb58db97408310481a9d1d1
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-12 17:04:25 +1100

    added apollo to fe

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { MstProvider } from './utils/MstProvider';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import {
 	ThemeProvider,
 	theme,
@@ -11,15 +12,22 @@ import {
 
 const root = document.getElementById('root') as HTMLElement;
 
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+});
+
 ReactDOM.unstable_createRoot(root).render(
 	<React.StrictMode>
-		<MstProvider>
-			<ThemeProvider theme={theme}>
-				<ColorModeProvider>
-					<CSSReset />
-					<App />
-				</ColorModeProvider>
-			</ThemeProvider>
-		</MstProvider>
+		<ApolloProvider client={client}>
+			<MstProvider>
+				<ThemeProvider theme={theme}>
+					<ColorModeProvider>
+						<CSSReset />
+						<App />
+					</ColorModeProvider>
+				</ThemeProvider>
+			</MstProvider>
+		</ApolloProvider>
 	</React.StrictMode>,
 );
