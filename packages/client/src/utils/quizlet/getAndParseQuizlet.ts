8970e986b5549838bc60155825ec48c66e1eb7ce
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-12 16:55:35 +1100

    added a fetch to the graphql server for the html of quizlet

--- packages/client/src/utils/quizlet/getAndParseQuizlet.ts
+++ packages/client/src/utils/quizlet/getAndParseQuizlet.ts
@@ -1,9 +1,26 @@
 import cheerio from 'cheerio';
 
+/**
+ * Fetches a quizlet page html, cause the cors prevents it from doing it in the browser
+ * I'm hitting my graphql server for that with simple fetch command,
+ * because for some reason I can't be bothered to implement a propper one...
+ * @param {string} url a url to fetch html from
+ */
 export const getQuizletPage = async (url: string) => {
-	const res = await fetch(url);
-	const text = await res.text();
-	return text;
+	const query = `query GetQuizletPage($url: String!) { getQuizletPage(url: $url) }`;
+	const res = await fetch(
+		process.env.NODE_ENV !== 'production' ? 'http://localhost:4000/' : '',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({ query, variables: { url } }),
+		},
+	);
+	const page = await res.text();
+	return page;
 };
 
 export const getQuizletWords = async (url: string) => {
