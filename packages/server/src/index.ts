57167c70798225ec27db609332f8f734b471bb50
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-13 23:55:12 +1100

    added different options for db connection

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import * as TypeORM from 'typeorm';
 import { buildSchema } from 'type-graphql';
+import dotenv from 'dotenv';
 
 import { Book } from './entities/Book';
 import { Language } from './entities/Language';
@@ -18,22 +19,30 @@ import { LanguageResolver } from './resolvers/Language';
 import { WordResolver } from './resolvers/Word';
 import { QuizletResolver } from './resolvers/Quizlet';
 
+const choose = <T>(dev: T, prod: T): T =>
+	process.env.NODE_ENV !== 'production' ? dev : prod;
+
 const bootstrap = async () => {
+	dotenv.config();
 	try {
 		// create TypeORM connection
 		await TypeORM.createConnection({
 			type: 'postgres',
-			database: 'cheats',
-			username: 'postgres', // fill this with your username
-			password: 'postgres', // and password
+			database: choose('cheats', process.env.DB_NAME),
+			username: choose('postgres', process.env.DB_USERNAME), // fill this with your username
+			password: choose('postgres', process.env.DB_PASSWORD), // and password
 			port: 5432, // and port
-			host: 'localhost', // and host
+			host: choose('localhost', 'ec2-3-220-23-212.compute-1.amazonaws.com'),
 			entities: [Book, Language, Sheet, Tag, User, Word, TranslationGroup],
 			synchronize: true,
-			logger: 'debug',
-			dropSchema: true,
+			logger: choose('debug', 'simple-console') as
+				| 'debug'
+				| 'advanced-console'
+				| 'simple-console'
+				| 'file',
+			dropSchema: process.env.NODE_ENV !== 'production',
 			cache: false,
-			logging: 'all',
+			logging: process.env.NODE_ENV !== 'production' ? 'all' : false,
 		});
 
 		// build TypeGraphQL executable schema
