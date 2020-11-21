00bf792403d5c4eff06f581dd18b64405c5bd2dc
Aleksander Brymora <aleksander.australia@gmail.com>
2020-11-21 23:16:15 +0100

    managed to build server to a single file for deployment

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.ts',
-	devtool: 'inline-source-map',
+	// devtool: 'inline-source-map',
 	module: {
 		rules: [
 			{
@@ -14,6 +14,17 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
+		fallback: {
+			crypto: require.resolve('crypto-browserify'),
+			os: require.resolve('os-browserify/browser'),
+			stream: require.resolve('stream-browserify'),
+			path: require.resolve('path-browserify'),
+			http: require.resolve('stream-http'),
+			zlib: require.resolve('browserify-zlib'),
+			fs: false,
+			net: false,
+			tls: false,
+		},
 	},
 	output: {
 		filename: 'bundle.js',
